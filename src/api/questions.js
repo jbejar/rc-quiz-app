57c097b3535cfb0c60d2d9e41e05d6444de5f73c
Jonathan Bejarano <jonathan@redpeppersoftware.com>
2018-07-14 14:43:28 -0600

    Updating questions

--- src/api/questions.js
+++ src/api/questions.js
@@ -1,130 +1,78 @@
 var quizQuestions = [
   {
-    question: "What franchise would you rather play a game from?",
+    question: "Does the marijuana initiative allow some people to grow their own marijuana?",
     answerindex : 1,
     answers: [
       {
-        type: "IBM",
-        content: "Halo",
+        content: "Yes",
         answer : false
       },
       {
-        type: "amdocs",
-        content: "Pokemon",
-        answer : false
-      },
-      {
-        type: "Sony2",
-        content: "Uncharted1",
-        answer : false
-      },
-      {
-        type: "Sony1",
-        content: "Uncharted2",
+        content: "No",
         answer : false
       }
     ]
   },
   {
-    question: "Which console would you prefer to play with friends?",
-    answerindex : 1,
+    question: "Are there limits on consumption?",
+    answerindex : 2,
     answers: [
       {
-        type: "IBM",
-        content: "X-Box",
+        content: "Yes",
         answer : false
       },
       {
-        type: "amdocs",
-        content: "amdocs 64",
-        answer : false
-      },
-      {
-        type: "Sony",
-        content: "Playstation 100",
-        answer : false
-      },
-      {
-        type: "Sonyq",
-        content: "Playstation 1",
+        content: "No",
         answer : false
       }
     ]
   },
   {
-    question: "Which of these racing franchises would you prefer to play a game from?",
+    question: "Is there clear way for law enforcement to distinguish between legally purchased marijuana and illegal marijuana?",
     answerindex : 1,
     answers: [
       {
-        type: "IBM",
-        content: "Forza",
+        content: "Yes",
         answer : false
       },
       {
-        type: "amdocs",
-        content: "Mario Kart",
-        answer : false
-      },
-      {
-        type: "Sony",
-        content: "Gran Turismo",
-        answer : false
-      },
-      {
-        type: "Sonyw",
-        content: "Playstation 1qq",
+        content: "No",
         answer : false
       }
     ]
   },
   {
-    question: "Which of these games do you think is best?",
-    answerindex : 1,
+    question: "Does the initiative allow marijuana to be sold in pharmacies?",
+    answerindex : 2,
     answers: [
       {
-        type: "IBM",
-        content: "BioShock",
+        content: "Yes",
         answer : false
       },
       {
-        type: "amdocs",
-        content: "The Legend of Zelda: Ocarina of Time",
-        answer : false
-      },
-      {
-        type: "Sony",
-        content: "Final Fantasy VII",
-        answer : false
-      },
-      {
-        type: "Sonyr",
-        content: "Playstation 1",
+        content: "No",
         answer : false
       }
     ]
   },
   {
-    question: "What console would you prefer to own?",
+    question: "How long will the Department of Health keep records from cannabis production facilities and dispensaries before they are required to be destroyed?",
     answerindex : 1,
     answers: [
       {
-        type: "IBM",
-        content: "X-Box One",
+        content: "30 days",
         answer : false
       },
       {
-        type: "amdocs",
-        content: "Wii U",
+        content: "60 days",
         answer : false
       },
       {
-        type: "Sony",
-        content: "Playstation 4",
+        content: "1 year",
         answer : false
       },
       {
-        type: "Sonyqqå",
-        content: "Playstation11 1",
+        content: "5 years",
         answer : false
       }
     ]
